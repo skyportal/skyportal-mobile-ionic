78e51ee7647f097ce9beac3fc3f88c3256e2d5b8
Joel DIBASSO <joel@dibasso.fr>
2024-05-14 17:19:10 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'skyportal-mobile-ionic',
+  webDir: 'dist'
+};
+
+export default config;
