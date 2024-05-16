87fdbbc36514b98d78b23840f33f0ed1a50010cd
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2024-05-16 11:15:38 -0700

    First screen (#1)
    * Add prettier and implement first screen
    
    * Add `start` script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended'
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier"
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn"
   }
 }
