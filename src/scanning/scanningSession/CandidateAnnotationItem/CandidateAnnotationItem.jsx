5cc65faf7b91dbc3965584b642a411fadd0099dd
Joël DIBASSO <51450344+dj-256@users.noreply.github.com>
2024-09-10 12:16:45 -0700

    Remove detail indicators from annotation lines (#66)
    * Remove detail indicators from annotation lines
    
    * Add indicators in pinned annotations and the annotation list
    
    * Indicate when there is no value for an annotation key in the PinnedAnnotations.jsx panel
    
    * Add an icon to nudge the user into copying an annotation value in both the pinned annotations and the annotation list

--- src/scanning/scanningSession/CandidateAnnotationItem/CandidateAnnotationItem.jsx
+++ src/scanning/scanningSession/CandidateAnnotationItem/CandidateAnnotationItem.jsx
@@ -1,6 +1,14 @@
 import "./CandidateAnnotationItem.scss";
-import { IonItem, IonLabel, IonList, IonListHeader } from "@ionic/react";
+import {
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonText,
+} from "@ionic/react";
 import { useCopyAnnotationLineOnClick } from "../../scanningLib.js";
+import { copyOutline } from "ionicons/icons";
 
 /**
  * @param {Object} props
@@ -24,9 +32,16 @@ export const CandidateAnnotationItem = ({ annotation }) => {
             <IonItem
               key={key}
               onClick={() => handleTextCopied(key, value)}
+              detail={false}
               button
             >
-              {key}: {value}
+              <IonText color="secondary">{key}:</IonText>
+              {"\u00A0"}
+              <IonText>{value}</IonText>
+              {"\u00A0"}
+              {"\u00A0"}
+              {"\u00A0"}
+              <IonIcon icon={copyOutline} size="small" color="secondary" />
             </IonItem>
           ))}
       </IonList>
