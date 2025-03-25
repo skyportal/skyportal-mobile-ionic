230f3b92d7913df502007cd3560685bf7ff3fd19
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-25 11:53:20 -0500

    Refactor Classification component to return null when no recent human classification is available

--- src/scanning/scanningSession/components/Classification/Classification.jsx
+++ src/scanning/scanningSession/components/Classification/Classification.jsx
@@ -4,23 +4,25 @@ import { IonChip } from "@ionic/react";
 /**
  * @param {Object} props
  * @param {import("../../../scanning.lib.js").Candidate} props.candidate
- * @returns {JSX.Element}
+ * @returns {JSX.Element | null}
  */
 export const Classification = ({candidate}) => {
   const mostRecentHumanClassification =
     candidate.classifications?.filter((c) => (c?.ml === false || c?.ml === null) && c.probability > 0)
       ?.sort((a, b) => b.modified.localeCompare(a.modified))[0]?.classification || null;
 
+  if (!mostRecentHumanClassification) {
+    return null;
+  }
+
   return (
     <div className="classification">
       Latest classification
-      {mostRecentHumanClassification && (
-        <div>
-          <IonChip color="primary">
-            {mostRecentHumanClassification}
-          </IonChip>
-        </div>
-      )}
+      <div>
+        <IonChip color="primary">
+          {mostRecentHumanClassification}
+        </IonChip>
+      </div>
     </div>
   );
 };
