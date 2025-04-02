3aa585df77f9cbe118699bb7866c77be5c3b335a
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-04-02 16:41:29 -0500

    Move getDateDiff function to scanning.lib.js for better modularity and reuse in Comments component.

--- src/scanning/scanning.lib.js
+++ src/scanning/scanning.lib.js
@@ -693,3 +693,32 @@ export const sanitizeAnnotationData = (data, withIndentation) => {
   }
   return data;
 }
+
+/**
+ * @param {string} stringUTCDate
+ * @returns {string}
+ */
+export const getDateDiff = (/** @type {string} */stringUTCDate) => {
+  const date = new Date(stringUTCDate + "Z"); // Add 'Z' to indicate that the date is in UTC
+  if (isNaN(date.getTime())) {
+    return "...";
+  }
+  const diff = new Date().getTime() - date.getTime();
+  const diffInMinutes = Math.floor(diff / (1000 * 60));
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} minute${diffInMinutes > 1 ? "s" : ""} ago`;
+  }
+
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} hour${diffInHours > 1 ? "s" : ""} ago`;
+  }
+
+  const diffInDays = Math.floor(diffInHours / 24);
+  if (diffInDays < 365) {
+    return `${diffInDays} day${diffInDays > 1 ? "s" : ""} ago`;
+  }
+
+  const diffInYears = Math.floor(diffInDays / 365);
+  return `${diffInYears} year${diffInYears > 1 ? "s" : ""} ago`;
+}
--- src/scanning/scanningSession/components/Comments/Comments.jsx
+++ src/scanning/scanningSession/components/Comments/Comments.jsx
@@ -1,36 +1,12 @@
 import "./Comments.scss"
 import { IonAccordion, IonAccordionGroup, IonItem, IonLabel, IonText } from "@ionic/react";
+import { getDateDiff } from "../../../scanning.lib.js";
 
 /**
  * @param {Object} props
  * @param {import("../../../scanning.lib.js").Comment[]} props.comments
  */
 export const Comments = ({comments}) => {
-  const getDateDiff = (/** @type {string} */stringUTCDate) => {
-    const date = new Date(stringUTCDate + "Z"); // Add 'Z' to indicate that the date is in UTC
-    if (isNaN(date.getTime())) {
-      return "...";
-    }
-    const diff = new Date().getTime() - date.getTime();
-    const diffInMinutes = Math.floor(diff / (1000 * 60));
-    if (diffInMinutes < 60) {
-      return `${diffInMinutes} minute${diffInMinutes > 1 ? "s" : ""} ago`;
-    }
-
-    const diffInHours = Math.floor(diffInMinutes / 60);
-    if (diffInHours < 24) {
-      return `${diffInHours} hour${diffInHours > 1 ? "s" : ""} ago`;
-    }
-
-    const diffInDays = Math.floor(diffInHours / 24);
-    if (diffInDays < 365) {
-      return `${diffInDays} day${diffInDays > 1 ? "s" : ""} ago`;
-    }
-
-    const diffInYears = Math.floor(diffInDays / 365);
-    return `${diffInYears} year${diffInYears > 1 ? "s" : ""} ago`;
-  }
-
   return (
     <div className="comments section">
       <div className="section-title section-padding">
